1e978ccd20ccada0ce19b53728d7a5d7cf9741fb
Muhammad Aizuddin Zali <mzali@mzali-fedora.redhat.com>
2020-03-02 19:48:59 +0800

    billinfo update

--- flaskapps/billinfo/wsgi.py
+++ flaskapps/billinfo/wsgi.py
@@ -6,18 +6,18 @@ user1bill = [ {'Due Date': '25th July 2020'},
           {'Amount' : 'USD 120.25'}
         ]
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def home():
   return "Im serving!"
 
-@app.route("/user1", methods=['GET'])
+@application.route("/user1", methods=['GET'])
 def user1info():
   return jsonify({'userbill' : user1bill})
 
 if __name__ == "__main__":
-  app.run()
+  application.run()
 
 
