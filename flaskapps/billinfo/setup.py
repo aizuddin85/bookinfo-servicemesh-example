660ca1adc6607d20b9e2f49b1ee95a45f0382c36
Muhammad Aizuddin Zali <mzali@mzali-fedora.redhat.com>
2020-03-02 18:43:52 +0800

    billinfo update

---
+++ flaskapps/billinfo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='BillInfo',
+      version='1.0',
+      description='Simple flask api on OCP',
+      author='Aizuddin Zali',
+      author_email='mzali@redhat.com',
+      url='https://github.com/aizuddin85/bookinfo-servicemesh-example',
+      install_requires=['Flask'],
+     )
